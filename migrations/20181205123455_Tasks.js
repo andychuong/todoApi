a4f702b2f02c5a9abe7492ac1f060a9384b03589
Andy Chuong <andy.chuong@colorado.edu>
2018-12-05 15:46:26 -0700

    redo migrations to add 'done'

--- migrations/20181205123455_Tasks.js
+++ migrations/20181205123455_Tasks.js
@@ -2,6 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('tasks', table => {
       table.increments();
       table.string('task').notNullable();
+      table.boolean('done').defaultsTo(false);
   })
 };
 
