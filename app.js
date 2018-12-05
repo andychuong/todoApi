d9927332146b7b1a89a7cf761ed537b10e8ad259
Andy Chuong <andy.chuong@colorado.edu>
2018-12-05 16:31:45 -0700

    cors

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ var tasksRouter = require('./routes/tasks');
 
 var app = express();
 
-app.use(cors());
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
